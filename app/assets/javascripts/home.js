897efe4cf60497fc705d6e16f37959eaa034ff5d
William Holt <wtholt27@gmail.com>
2015-03-05 22:28:57 -0500

    fixed error

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,6 @@
 var contactList = angular
   .module('contactList', [])
-  .config(['$httpProvider']function($httpProvider){
+  .config(['$httpProvider', function($httpProvider){
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $("meta[name=csrf-token]").attr("content");
   }])
 ;
